8e9f3cab3b38a38363aa08e79ba094f5b74aa5f7
Alina <buchina-alina@mail.ru>
2024-02-20 20:06:20 +1000

    Справочник

---
+++ ui.py
@@ -0,0 +1,13 @@
+from logger import input_data, print_data
+
+def interfase()
+    print("Добрый день! Добро пожаловать на специальный бот справочник! \n 1 - запись данных \n 2 -ввод данных")
+    command = int(input('Введите число'))
+
+    while command !=1 and command != 2
+          print("Неправильный ввод")
+          command = int(input('Введите число'))
+    if command ==1
+       input data()
+    elif command == 2
+         print data      
