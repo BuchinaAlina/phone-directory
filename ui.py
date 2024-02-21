a443913b11deeb1a4895c565e516d520c86fe5ab
Alina <buchina-alina@mail.ru>
2024-02-21 19:24:04 +1000

    gj

--- ui.py
+++ ui.py
@@ -4,10 +4,12 @@ def interface():
     print("Добрый день! Добро пожаловать на специальный бот справочник! \n 1 - запись данных \n 2 -ввод данных")
     command = int(input('Введите число'))
 
-    while command !=1 and command != 2
+    while command !=1 and command != 2:
           print("Неправильный ввод")
           command = int(input('Введите число'))
-    if command ==1
-       input data()
-    elif command == 2
-         print data      
+    if command ==1:
+       input_data()
+    elif command == 2:
+         print_data   
+
+
